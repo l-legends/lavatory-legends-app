bd19d7fd772fbfef24c77d541f4d9e4d832404cb
Pauline Cvt <paulinecvt2021@gmail.com>
2024-02-16 11:41:25 +0100

    installed router

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from from "react-router-dom";
-import Logo from "./img/logo.png";
+import { Link } from "react-router-dom";
+import logo from "../img/logo.png";
 
 
 function Footer() {
@@ -10,7 +10,7 @@ function Footer() {
             <div className="Footer">
                 <p>© 2024 Lavatory Legends. All rights reserved.</p>
                 <Link to="/">
-                    <img src={Logo} alt="Logo" />
+                    <img src={logo} alt="Logo" />
                 </Link>
                 <Link to="https://github.com/l-legends/lavatory-legends-app">GitHub Repo</Link>
             </div >
