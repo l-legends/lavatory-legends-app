d569194cfbef3d6b2bbefb768c59b50110e81856
Pauline Cvt <paulinecvt2021@gmail.com>
2024-02-16 16:38:59 +0100

    DetailsPage is functionnal and display the content depending of the item's id

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
 
-function DetailsPage () {
+function DetailsPage() {
+    const database = "https://lavatory-legends.adaptable.app/lavatories"
+
+    const [lavatory, setLavatory] = useState(null);
+    const { id } = useParams();
+
+    const getLavatory = () => {
+        axios.get(`${database}/${id}`)
+            .then((response) => {
+                setLavatory(response.data);
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+    };
+    useEffect(() => {
+        getLavatory();
+    }, [id]);
+
+
+
 
     return (
         <>
-        <h1>Your favorite lavatory</h1>
-        <p>Next stop: here.</p>
+            <div className="ProjectDetailPage">
+                {lavatory === null
+                    ? <p>Lavatories loading...</p>
+                    : (
+                        <>
+                            <img src={lavatory.imageURL} />
+                            <br/>
+                            <label>{lavatory.tags[0]} {lavatory.tags[1]} {lavatory.tags[2]} {lavatory.tags[3]}</label>
+                            <h1>{lavatory.title}</h1>
+                            <p>{lavatory.description}</p>
+                            <p>{lavatory.location.place}</p>
+                            <p>{lavatory.location.city}</p>
+                            <p>{lavatory.location.country}</p>
+                        </>
+                    )
+                }
+
+
+
+            </div>
         </>
 
     );
