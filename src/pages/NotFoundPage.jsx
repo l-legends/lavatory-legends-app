bb523b166f9aeef05a13b14d8b4e873d3db5fb4b
Simona Wurst <simona_wu@yahoo.de>
2024-02-20 19:02:27 +0100

    Initial setup 404 page - img still missing

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,15 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+//import Pauline from '../img/pauline-about.png';
+//import Simona from '../img/simona-about.png';
+import GitHub from '../img/github-logo.png';
 
-function NotFoundPage () {
+function NotFound() {
+
+    const [clickCountPauline, setClickCountPauline] = useState(0)
+    const [clickCountSimona, setclickCountSimona] = useState(0)
+
+    const handlePaulineClick = (e) => {
+        e.preventDefault();
+        setClickCountPauline(clickCountPauline + 1);
+    };
+
+    const handleSimonaClick = (e) => {
+        e.preventDefault();
+        setclickCountSimona(clickCountSimona + 1);
+    };
+
 
     return (
-        <>
-        <h1>We messed up again, you idiot!!</h1>
-        <p>Beer Challenge?</p>
-        </>
-
+        <div className="NotFoundPage">
+                <h1>We messed up, meow!</h1>
+                <p>Blame who's in charge. The other one gets a free beer.</p>
+                <div className="NotFoundConatiner">
+                    <div className="Pauline404">
+                        <img src={GitHub} alt="Pauline" onClick={handlePaulineClick} />
+                        <p>Pauline owes Simona {clickCountPauline} beer(s).</p>
+                    </div>
+                    <div className="Simona404">
+                        <img src={GitHub} alt="Simona" onClick={handleSimonaClick} />
+                        <p>Simona owes Pauline {clickCountSimona} beer(s).</p>
+                    </div>
+                </div>   
+                <Link to="/">Let's go back to discover all lavatories</Link>
+        </div>
     );
 }
 
-export default NotFoundPage;
+export default NotFound;
