e7078256d9634257a2af1c1517bec0132701fb0d
Simona Wurst <simona_wu@yahoo.de>
2024-02-23 12:08:13 +0100

    Style fix homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ function HomePage() {
         <>
             <section className="hero">
                 <div>
-                    <h1 className="text-5xl text-white font-league"> Welcome to the Lavatory Parade </h1>
+                    <h1 className="text-5xl text-white font-league mx-5"> Welcome to the Lavatory Parade </h1>
                     <br />
                     <p className="text-white my-4 py-3 bg-orange bg-opacity-70">The Place to Pee.
                     </p>
@@ -19,7 +19,7 @@ function HomePage() {
                 <SearchBar />
             </section>
             <section>
-                <h2 className="text-3xl text-grey font-league py-9">Find pees. Set priorities.</h2>
+                <h2 className="text-3xl text-grey font-league pt-14 pb-10">Find pees. Set priorities.</h2>
                 <FilterByTags />
                 <ListAll />
             </section>
